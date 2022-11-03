b5262fd01285f250dfb78d52ca5f38eaaca8c807
Masatingarrix24 <104561540+Masatingarrix24@users.noreply.github.com>
2022-11-03 13:09:23 +0900

    Update fizzbuzz.php

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -3,11 +3,11 @@
     if ( $i % 3 == 0 && $i % 5 == 0 ){
 	echo "FizzBuzz\n";
     
-    } elseif ( $i % 5 == 0 ){
-        echo "Buzz\n";
-    
     } elseif( $i % 3 == 0 ){
         echo "Fizz\n";
+	    
+    } elseif ( $i % 5 == 0 ){
+        echo "Buzz\n";
  
     } else {
         echo $i."\n";
